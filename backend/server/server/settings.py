211cdb4e8807aa6ae787a641bbb6f05590272935
Michael Perez <michaelperezabreu@gmail.com>
2022-07-25 21:32:47 -0400

    Added .env file for protecting secret key

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91ffb8252b5c67b847ea41aa3560da80380a28d82e3c77e98ea8457348c53cb9
-size 3318
+oid sha256:4f246dfdbcc28d228dcf83bdad34c6217083126094068cbd989f37f3faa11c05
+size 3346
